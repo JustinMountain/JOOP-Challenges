fc2577417ada79c62999aabe1d5c7e353acf1213
Justin Mountain <moun0048@algonquinlive.com>
2022-06-03 11:13:39 -0400

    Refactored and added Javadoc comments

--- 22S-Challenges/src/digital_root/DigitalRoot.java
+++ 22S-Challenges/src/digital_root/DigitalRoot.java
@@ -1,17 +1,35 @@
 package digital_root;
 
-// https://www.codewars.com/kata/541c8630095125aba6000c00
+/**
+ * The DigitialRoot class calculates the digital root of input numbers.
+ * Problem from: https://www.codewars.com/kata/541c8630095125aba6000c00
+ * @author Justin Mountain
+ */
 public class DigitalRoot {
 
+	/**
+	 * The main method to the program calls the sumOfDigits function on each int in the array testWords
+	 * @param args Command line arguments are not used by this program
+	 */
 	public static void main(String[] args) {
-		int testNumber = 493193;
+		int[] testNumbers = { 16, 942, 132189, 493193 };
 		
-		sumOfDigits(testNumber);
+		for (int i = 0; i < testNumbers.length; i++) {
+			
+			sumOfDigits(testNumbers[i]);
+			System.out.println();
+		}
 	}
 	
-	// Recursive method to find the digital root
+	/**
+	 * Recursively determines the digital root for the input integer
+	 * @param input integer to find the digital root for
+	 * @return
+	 */
 	public static int sumOfDigits(int input) {
 		String inputAsString = Integer.toString(input);
+		
+		System.out.printf("%n%8d --> ", input); // Outputs the input number to the console
 				
 		if (inputAsString.length() == 1) { // Digital root found if input has only 1 digit
 			return input;
@@ -23,14 +41,14 @@ public class DigitalRoot {
 				int value = Character.getNumericValue(charArray[i]);	
 				sum += value;
 				
-				if (i ==  charArray.length - 1) {
+				if (i ==  charArray.length - 1) { // Formatting for console output
 					System.out.print(value + " ");
 				} else {
 					System.out.print(value + " + ");
 
 				}
 			}
-			System.out.println(" = " + sum);
+			System.out.print(" = " + sum);
 			
 			// If sum has 1 digit return sum, else recurse with new sum
 			return (sum < 9) ? sum : (sum = sumOfDigits(sum));
